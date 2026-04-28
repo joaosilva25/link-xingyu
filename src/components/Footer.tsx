e555a14efb9d87d988db25cf67a5061b6ea8c213
João Vitor <joaosilva2505@outlook.com>
2026-04-28 18:46:26 -0300

    fix: remove img footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,6 @@ const Footer = () => {
     <footer className="bg-white py-5">
       <div className="max-w-7xl mx-auto px-6 py-8 flex flex-col items-center gap-4">
         <div className="flex flex-col items-center gap-3">
-          <img src={logo} alt="Xingyu" className="w-18 h-18 object-contain" />
           <a href="https://www.xingyu.com.br" className="text-black/70 text-xm hover:underline">www.xingyu.com.br</a>
         </div>
         <p className="text-black/70 text-xs">
