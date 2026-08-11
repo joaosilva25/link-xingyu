147e212cc80e67a8c4eda97db8c215722e8e9fe3
João Vitor <joaosilva2505@outlook.com>
2026-08-11 18:53:44 -0300

    fixx: adjust

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,29 +1,23 @@
 import Card from './Card';
+import bannerTrend from '../assets/BANNER CHINANOBRASIL.png';
+import bannerTop from '../assets/Live.png';
 import banner2 from '../assets/BANNER 02.png';
 import banner5 from '../assets/BANNER 05.png';
 import banner10 from '../assets/BANNER 10.png';
-import { useTrendBanner } from '../hooks/useTrendBanner';
-
-function BannerSkeleton() {
-  return (
-    <div
-      aria-hidden
-      className="w-full h-[210px] md:h-[400px] rounded-3xl border-4 border-orange-200 bg-orange-50 animate-pulse"
-    />
-  );
-}
 
 export default function Links() {
-  const { config: trendBanner, isLoading: isTrendLoading } = useTrendBanner();
-
   return (
     <section id="links" className="bg-white">
       <div className="max-w-6xl mx-auto gap-12 flex flex-col pb-4 md:pb-24 pt-0 md:pt-14 px-2">
-        {isTrendLoading || !trendBanner ? (
-          <BannerSkeleton />
-        ) : (
-          <Card imageSrc={trendBanner.imageUrl} link={trendBanner.link} />
-        )}
+
+        <Card
+          imageSrc={bannerTop}
+          link="https://www.xingyu.com.br/collections/brilho-e-sucesso"
+        />
+                <Card
+          imageSrc={bannerTrend}
+          link="https://chinanobrasil.xingyujewelry.com.br/?utm_source=BIOINSTA&utm_medium=PAGCAPTURA&utm_campaign=CHINANOBRASIL&utm_id=LANCAMENTO"
+        />
         <Card imageSrc={banner2} link="https://www.xingyu.com.br" />
         <Card imageSrc={banner5} link="https://vip.xingyujewelry.com.br/" />
         <Card imageSrc={banner10} link="https://wa.me/+5511925694819" />
