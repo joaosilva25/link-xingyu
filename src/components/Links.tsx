044496cd4bb9f070c90f215a06dd036892f9439c
joaosilva25 <joaosilva2505@outlook.com>
2026-09-24 13:27:15 -0300

    fix: change banner

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import Card from './Card';
 import bannerBlackFriday from '../assets/BANNER BIO BlackFriday.png';
-import bannerLancamento from '../assets/BANNERSEMIJOIASIMPORTADAS.png';
+import bannerLancamento from '../assets/BANNER BIO - Lunar.png';
 import banner2 from '../assets/BANNER 02.png';
 import banner5 from '../assets/BANNER 05.png';
 import banner10 from '../assets/BANNER 10.png';
@@ -59,13 +59,13 @@ export default function Links() {
           imageSrc={bannerOrigem}
           link="https://www.xingyu.com.br/collections/origem?filter.v.availability=1&filter.v.price.gte=&filter.v.price.lte=&sort_by=created-descending&utm_source=BANNERBIO&utm_medium=COLECAO07&utm_campaign=INSTAGRAM&utm_id=CHINANOBRASIL"
         /> */}
+        <Card
+          imageSrc={bannerLancamento}
+          link="https://www.xingyu.com.br/collections/colecao-lunar?utm_source=BANNER&utm_medium=INSTABIO&utm_campaign=10SI&utm_id=COLECAOLUNAR"
+        />
            <Card
           imageSrc={bannerBlackFriday}
           link="http://ab.xingyujewelry.com.br/?utm_source=BANNERINSTA&utm_medium=BIOCAPTURA&utm_campaign=11AB&utm_id=LANCAMENTO"
-        />
-          <Card
-          imageSrc={bannerLancamento}
-          link="http://si.xingyujewelry.com.br/?utm_source=BANNERBIO&utm_medium=PAGCAPTURA&utm_campaign=10SI&utm_id=LANCAMENTO"
         />
         <Card imageSrc={banner2} link="https://www.xingyu.com.br" />
         <Card imageSrc={banner5} link="https://vip.xingyujewelry.com.br/" />
